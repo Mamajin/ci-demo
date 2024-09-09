fb2e8533dc13c544f37c1dd11498563186327ff2
Phasit Ruangmak <phasit2005@gmail.com>
2024-09-09 15:03:01 +0700

    Code styling completed

--- statistics.py
+++ statistics.py
@@ -1,7 +1,4 @@
-"""
-This file contains Statistic calculation functions for the user to
-implement in their program.
-"""
+"""This file contains Statistic calculation functions."""
 
 from math import sqrt
 
@@ -14,7 +11,7 @@ def average(data):
 
 
 def variance(data):
-    """The population variance of a list of numbers in data.
+    """Return population variance of a list of numbers in data.
 
     The variance is the sum of squared differences between data values
     and their mean, divided by the number of items in the list.
@@ -22,9 +19,8 @@ def variance(data):
     which returns the sample variance, where the sum is divided by (n-1).
 
     Example: variance([1,5]) is ((1-3)**2 + (5-3)**2)/2 = 4.
-
-    :param data: list of numbers for which variance will be computed. 
-           Must contain at least one element.
+    :param data: list of numbers for which variance will be computed.
+    Must contain at least one element.
     :returns: population variance of values in data list.
     :raises ValueError: if the data parameter is empty.
 
@@ -45,7 +41,7 @@ def variance(data):
 
 
 def stdev(data):
-    """The standard deviation of a list of values."""
+    """Return standard deviation of a list of values."""
     if len(data) == 0:
         raise ValueError("List must contain at least one value")
     if len(data) == 1:
